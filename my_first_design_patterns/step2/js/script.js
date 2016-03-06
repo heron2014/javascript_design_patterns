612c4539269983e21425f87f50f991574dfc572d
Anita <a.nita@hotmail.co.uk>
2016-03-06 20:17:42 +0000

    creating an one object and moving all global variables and functions into this object as its properties and methods

--- my_first_design_patterns/step2/js/script.js
+++ my_first_design_patterns/step2/js/script.js
@@ -1,49 +1,49 @@
-var leadself = 'Me: ',
-	leadcomputer = "PC: ",
-	aSaid= ["This is a Cyber Chat"],
-	msgYes = "Yes, that's a great idea.",
-	msgNo = "No, that must be a mistake.",
-	aSassyStuff = [
-		"Like mold on books, grow myths on history.",
-		"She moved like a poem and smiled like a sphinx.",
-		"As long as we don’t die, this is gonna be one hell of a story.",
-		"She laughed, and the desert sang.",
-		"You’ve got about as much charm as a dead slug."
-	];
+//what is namespace
 
 
-//these are 3 public methods
-function talk (msg) {
-	echo(leadself + msg);
+//create object literal,make sure to reference right properties by using 'this'
+var o = {
+  leadself : 'Me: ',
+  leadcomputer : "PC: ",
+  aSaid : ["This is a Cyber Chat"],
+  msgYes : "Yes, that's a great idea.",
+  msgNo : "No, that must be a mistake.",
+  aSassyStuff : [
+    "Like mold on books, grow myths on history.",
+    "She moved like a poem and smiled like a sphinx.",
+    "As long as we don’t die, this is gonna be one hell of a story.",
+    "She laughed, and the desert sang.",
+    "You’ve got about as much charm as a dead slug."
+  ],
+  talk : function (msg) {
+    this.echo(this.leadself + msg);
+  },
+  replyYesNo: function () {
+    var msg = Math.random() > 0.5 ? this.msgYes : this.msgNo;
+    this.echo(this.leadcomputer + msg);
+  },
+  saySassyStuff : function () {
+    var msg = this.aSassyStuff[ Math.floor(Math.random() * this.aSassyStuff.length)];
+    this.echo(this.leadcomputer + msg);
+  },
+  echo : function (msg) {
+    this.aSaid.push("<div>" + msg + "</div>");
+
+    //show only six chats , not more
+    var aSaidLength = this.aSaid.length,
+      start = Math.max(aSaidLength - 6, 0), //that value or 0 , avoid any negative numbers
+      out = '';
+
+      for (var i = start; i < aSaidLength; i++) {
+        out += this.aSaid[i];
+      }
+
+    $('.chat').html(out); 
+    $('#talk span').text(msg);
+  }
 }
 
-//computer replies
-function replyYesNo () {
-  var msg = Math.random() > 0.5 ? msgYes : msgNo;
-  echo(leadcomputer + msg);
-}
 
-function saySassyStuff () {
-  var msg = aSassyStuff[ Math.floor(Math.random() * aSassyStuff.length)];
-  echo(leadcomputer + msg);
-}
-
-//this one we want to be private method, not available to the user (but for now is public)
-function echo (msg) {
-  aSaid.push("<div>" + msg + "</div>");
-
-  //show only six chats , not more
-  var aSaidLength = aSaid.length,
-  	start = Math.max(aSaidLength - 6, 0), //that value or 0 , avoid any negative numbers
-  	out = '';
-
-  	for (var i = start; i < aSaidLength; i++) {
-  		out += aSaid[i];
-  	}
-
-  $('.chat').html(out);	
-  $('#talk span').text(msg);
-}
 
 
 
